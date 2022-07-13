736281735ed77f7c655838930feeaeb191a31124
Fabiano Alcantara <fabianoalcantarabr@yahoo.com.br>
2022-07-13 13:08:51 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SalesController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dsmeta.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmeta.entidades.Sales;
+import com.devsuperior.dsmeta.services.SalesService;
+
+@RestController
+@RequestMapping(value = "/Sales")
+public class SalesController {
+	
+	@Autowired
+	private SalesService service;
+	
+	@GetMapping
+	public List<Sales> findSales(){
+		return service.findSales();
+		
+		
+	}
+}
---
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SalesRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsmeta.entidades.Sales;
+
+public interface SalesRepository extends JpaRepository<Sales, Long>{
+
+}
